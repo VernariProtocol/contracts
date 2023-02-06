f905a08f3888c4052ef995d046b474b69cdcf8b1
jongregis <jon.gregis@gmail.com>
2023-02-05 18:13:06 -0600

    start testing manager

---
+++ test/Store.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+import "../contracts/proxy/UUPSProxy.sol";
+import "../contracts/StoreManager.sol";
+
+contract StoreTest is Test {
+    function setUp() public {}
+}
